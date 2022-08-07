a5a7c4d8dd5e05f9b376d047af18c4cdf6c18ef2
Kavin Valli <mail@kavin.me>
2022-08-07 12:24:30 +0530

    feat: faculty page

--- pages/faculty.js
+++ pages/faculty.js
@@ -1,5 +1,25 @@
-import ComingSoon from "../components/ComingSoon";
+import PageHeader from "../components/PageHeader";
+import HeadingWithIllustration from "../components/HeadingWithIllustration";
+import faculty from "../data/faculty.json";
+import PersonCard from "../components/PersonCard";
 
 export default function Faculty() {
-  return <ComingSoon />;
+  return (
+    <div className="px-5 md:px-20 mx-auto max-w-[1400px] w-full">
+      <PageHeader
+        title="Faculty"
+        description="The Exun faculty forms the bedrock of all of its activities."
+      />
+      {faculty.map((section) => (
+        <>
+          <HeadingWithIllustration title={section.title} />
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+            {section.members.map(({ name, role }) => (
+              <PersonCard key={name} name={name} role={role} />
+            ))}
+          </div>
+        </>
+      ))}
+    </div>
+  );
 }
