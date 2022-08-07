15194c68aa3ab900d5d7f3b87d7a74f63cc15292
Kavin Valli <mail@kavin.me>
2022-08-07 12:20:54 +0530

    feat: alumni page

--- components/PersonCard.js
+++ components/PersonCard.js
@@ -1,7 +1,7 @@
 export default function PersonCard({ name, role }) {
   return (
     <div className="w-full bg-[#F4F8FF] rounded-lg flex flex-col justify-center items-center p-8">
-      <h4 className="mb-2 text-xl font-semibold tracking-wider text-[#194A6D] mb-4">
+      <h4 className="mb-2 text-xl font-semibold tracking-wider text-[#194A6D] mb-3">
         {name}
       </h4>
       <h5 className="mb-2 text-lg font-semibold tracking-wider text-accent-dark">
--- pages/alumni.js
+++ pages/alumni.js
@@ -1,5 +1,25 @@
-import ComingSoon from "../components/ComingSoon";
+import PageHeader from "../components/PageHeader";
+import HeadingWithIllustration from "../components/HeadingWithIllustration";
+import alumni from "../data/alumni.json";
+import PersonCard from "../components/PersonCard";
 
 export default function Alumni() {
-  return <ComingSoon />;
+  return (
+    <div className="px-5 md:px-20 mx-auto max-w-[1400px] w-full">
+      <PageHeader
+        title="Alumni"
+        description="Exun has an immense, well-connected alumni network that provides mentorship and support to its members."
+      />
+      {alumni.map((batch) => (
+        <>
+          <HeadingWithIllustration title={batch.year} />
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+            {batch.alumni.map(({ name, role }) => (
+              <PersonCard key={name} name={name} role={role} />
+            ))}
+          </div>
+        </>
+      ))}
+    </div>
+  );
 }
