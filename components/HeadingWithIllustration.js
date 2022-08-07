dd80ae7e400706ced67777ff0cdc82cd3838e64d
Kavin Valli <mail@kavin.me>
2022-08-07 19:04:11 +0530

    fix: heading illustration overflowing issue and animation

--- components/HeadingWithIllustration.js
+++ components/HeadingWithIllustration.js
@@ -1,16 +1,24 @@
+import { motion } from "framer-motion";
+
 export default function HeadingWithIllustration({
   title,
   illustration = "/resources-illustration-1.png",
 }) {
   return (
-    <div className="my-12 relative flex justify-between items-center">
+    <div className="my-12 flex justify-between items-center">
       <h2 className="text-3xl font-semibold md:text-4xl text-accent">
         {title}
       </h2>
-      <img
+      <motion.img
+        viewport={{ once: true }}
+        // initial={{ transform: "translate(720px, 0px)" }}
+        // whileInView={{ transform: "translate(0px, 0px)" }}
+        initial={{ x: 600 }}
+        whileInView={{ x: 0 }}
+        transition={{ duration: 0.5 }}
         src={illustration}
         alt="Illustration"
-        className="absolute left-[720px] h-[70px]"
+        className="absolute right-0 h-[70px]"
       />
     </div>
   );
