c5bd53d2f45c821d2be5ee5246aa0d7633b787be
Kavin Valli <mail@kavin.me>
2022-08-07 12:32:56 +0530

    feat: contacts page

--- components/PersonCard.js
+++ components/PersonCard.js
@@ -1,10 +1,15 @@
-export default function PersonCard({ name, role }) {
+export default function PersonCard({ name, subtitle, role }) {
   return (
     <div className="w-full bg-[#F4F8FF] rounded-lg flex flex-col justify-center items-center p-8">
-      <h4 className="mb-2 text-xl font-semibold tracking-wider text-[#194A6D] mb-3">
+      <h4 className="text-xl font-semibold tracking-wider text-[#194A6D]">
         {name}
       </h4>
-      <h5 className="mb-2 text-lg font-semibold tracking-wider text-accent-dark">
+      {subtitle && (
+        <h5 className="text-lg font-regular tracking-wider text-accent-dark text-center">
+          {subtitle}
+        </h5>
+      )}
+      <h5 className="mb-2 text-lg font-semibold tracking-wider text-accent-dark mt-3">
         {role}
       </h5>
     </div>
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,21 @@
-import ComingSoon from "../components/ComingSoon";
+import PageHeader from "../components/PageHeader";
+import contacts from "../data/contacts.json";
+import PersonCard from "../components/PersonCard";
+
+console.log(contacts);
 
 export default function Contact() {
-  return <ComingSoon />;
+  return (
+    <div className="px-5 md:px-20 mx-auto max-w-[1400px] w-full">
+      <PageHeader
+        title="Contact Us"
+        description="Members of Exun Clan are really accessible and very easy to reach out to."
+      />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+        {contacts.map(({ name, role, email }) => (
+          <PersonCard key={name} name={name} subtitle={role} role={email} />
+        ))}
+      </div>
+    </div>
+  );
 }
