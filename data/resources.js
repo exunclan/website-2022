d176580fc0913ef2ba49ab0be559fca0c795aae3
Kavin Valli <mail@kavin.me>
2022-08-08 14:41:09 +0530

    chore: change resource images

--- data/resources.js
+++ data/resources.js
@@ -7,98 +7,98 @@ export const resources = [
     link: "https://exun.co/resources/gamedev",
   },
   {
-    image: "gamedev.png",
+    image: "cross.png",
     name: "Crossword",
     description:
       "Crossword is a relatively unconventional department and definitely not what you’ll initially think of when you hear the word. Luckily enough, one doesn’t require too much in-depth knowledge of any technical fields for crossword, since it’s mainly intuitive thus making it interesting for anyone and everyone.",
     link: "https://exun.co/resources/cross",
   },
   {
-    image: "gamedev.png",
+    image: "design.png",
     name: "Web Development",
     description:
       "This is a resourceful document for aspiring web developers and those looking to sharpen their skills alike. Listed tutorials assume no prior knowledge except knowing how to use the internet.",
     link: "https://exun.co/resources/dev",
   },
   {
-    image: "gamedev.png",
+    image: "design.png",
     name: "Design",
     description:
       "Design consists of designing everything from logos, posters, infographics, banners, print design, illustrations, UI/UX design for websites and apps, to animations and interactions",
     link: "https://exun.co/resources/design",
   },
   {
-    image: "gamedev.png",
+    image: "gd.png",
     name: "Group Discussion",
     description:
       "Every decision that is made by any institution/individual is made by considering various possibilities, and it is important for one to be able to analyse a topic/situation effectively to form logical decisions.",
     link: "https://exun.co/resources/gd",
   },
   {
-    image: "gamedev.png",
+    image: "design.png",
     name: "3D Design",
     description:
       "When you encounter the phrase “3D Design”, what goes through your mind? You may think back to animated films you’ve seen made by Pixar & Dreamworks or perhaps you think of the visual effects and CGI in films such as the Avengers franchise. But it's a lot more than that...",
     link: "https://exun.co/resources/3d",
   },
   {
-    image: "gamedev.png",
+    image: "cp.png",
     name: "Competitive Programming",
     description:
       '<i>"Programming… Competitive Programming… It teaches you how to think?."</i> ~Steve Jobs<br /> Competitive Programming is a mental sport which enables you to code a given problem under provided constraints.',
     link: "https://exun.co/resources/cp",
   },
   {
-    image: "gamedev.png",
+    image: "quiz.png",
     name: "Quizzing",
     description:
       "It's not a one line question with multiple choices for the answer; it's more of a paragraph-long puzzle, where you connect subtle hints within the question and apply your knowledge of technology to come to an answer.",
     link: "https://exun.co/resources/quiz",
   },
   {
-    image: "gamedev.png",
+    image: "cl.png",
     name: "Computational Linguistics",
     description:
       "Is it about knowing multiple languages? No!<br />Is it about being more proficient than anyone else in a single language? No!<br />Is it about how AI assistants understand us, how shakkar and sugar come actually from the same origin and how a child learns how to speak? Yes!",
     link: "https://exun.co/resources/cl",
   },
   {
-    image: "gamedev.png",
+    image: "ml.png",
     name: "Machine Learning",
     description:
       'You’ve probably heard the term "Artificial Intelligence" in the past, but have you ever wondered, how does it work? How is it made?',
     link: "https://exun.co/resources/ml",
   },
   {
-    image: "gamedev.png",
+    image: "design.png",
     name: "Mobile App Development",
     description:
       "Mobile Application development is the process of creating software applications which run on mobile devices and involves both UI and backend development (refer to Web Development resources).",
     link: "https://exun.co/resources/appdev",
   },
   {
-    image: "gamedev.png",
+    image: "hw.png",
     name: "Hardware",
     description:
       "Ever wondered how your computer works? Or ever tried to fix an issue with your hardware? Competitive hardware generally deals with troubleshooting computer hardware and going in-depth about its concepts.",
     link: "https://exun.co/resources/hardware",
   },
   {
-    image: "gamedev.png",
+    image: "hunt.png",
     name: "Cryptic Hunts",
     description:
       "Cryptic hunts are the online equivalent of treasure hunts and require you to hunt the internet for clues to solve given questions, using a plethora of online tools – cipher decoders/encoders and other common methods of data relaying like barcodes, QR codes, etc.",
     link: "https://exun.co/resources/cryptic",
   },
   {
-    image: "gamedev.png",
+    image: "design.png",
     name: "Motion Design",
     description:
       "When we think of motion design, we mostly think of “design that moves”. Although that is partly accurate, it doesn't do the concept of Motion design much justice... It is a process of video production through the use of animation, text and visual effects.",
     link: "https://exun.co/resources/motiondesign",
   },
   {
-    image: "gamedev.png",
+    image: "design.png",
     name: "Audio Editing",
     description:
       "Audio editing usually deals with making your own music or taking an original song made by someone else and giving it your own spin (remixing).",
