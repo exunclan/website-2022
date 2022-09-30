814966bf798e3cb650d0a6c615f53345a45d28da
Kavin Valli <mail@kavin.me>
2022-09-30 19:52:28 +0530

    chore: formatting and youtube thumbnail parse

--- components/home/Videos.js
+++ components/home/Videos.js
@@ -1,39 +1,43 @@
 import Image from "next/image";
-import youtubeThumbnail from 'youtube-thumbnail';
-import videoData from '../../data/videos'
+import videoData from "../../data/videos";
 
 export default function Videos({ aboutRef }) {
-    console.log(videoData)
-    return (
-        <>
-            <div className="w-full" ref={aboutRef}>
-                {videoData.map((element) => {
-                    return (
-                        <>
-                            <h2 className="text-4xl mt-24 text-primary text-left font-bold">
-                                Exun {element.year} Videos
-                            </h2>
-                            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
-                                {element.data.map((item) => {
-                                    return (
-                                        <>
-                                            <div className="flex flex-col p-6 mx-12">
-                                                <img
-                                                    className="object-cover h-52 lg:h-64"
-                                                    src={youtubeThumbnail(item.link).high.url}
-                                                    alt="Video Image"
-                                                />
-                                                <a href={item.link} className="bg-blue-500 inline-block text-white font-semibold rounded cursor-pointer py-4 text-center">{item.heading}</a>
-                                            </div>
-                                        </>
-                                    )
-                                })}
-                            </div>
-                        </>
-                    )
+  return (
+    <>
+      <div className="w-full" ref={aboutRef}>
+        {videoData.map((element) => {
+          return (
+            <>
+              <h2 className="text-4xl mt-24 text-primary text-left font-bold">
+                Exun {element.year} Videos
+              </h2>
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
+                {element.data.map((item) => {
+                  return (
+                    <>
+                      <div className="flex flex-col p-6 mx-2 sm:mx-12">
+                        <img
+                          className="object-cover h-52 lg:h-64"
+                          src={`https://img.youtube.com/vi/${
+                            item.link.split("v=")[1]
+                          }/hqdefault.jpg`}
+                          alt="Video Image"
+                        />
+                        <a
+                          href={item.link}
+                          className="bg-blue-500 inline-block text-white font-semibold rounded cursor-pointer py-4 text-center"
+                        >
+                          {item.heading}
+                        </a>
+                      </div>
+                    </>
+                  );
                 })}
-            </div>
-        </>
-    );
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 }
-  
