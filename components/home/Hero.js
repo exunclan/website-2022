e5b86204f7b94893c427abaaf3df370e751b4032
Kavin Valli <mail@kavin.me>
2022-11-05 16:26:17 +0530

    devfolio and co

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -10,22 +10,8 @@ export default function Hero({ scrollToAboutRef }) {
           <br /> <span className="text-primary">Exun</span>
         </h1>
         <div className="flex items-center gap-3 mt-5">
-          <button className="button flex items-center" onClick={scrollToAboutRef}>
+          <button className="button" onClick={scrollToAboutRef}>
             Read More
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13 7l5 5m0 0l-5 5m5-5H6"
-              />
-            </svg>
           </button>
           <a href="//reg.exun.co/invite" className="button text-center">
             Register for Exun 2022
--- components/home/Sponsors.js
+++ components/home/Sponsors.js
@@ -14,70 +14,101 @@ const sponsors = [
   },
   */
   {
-    imageUrl: "/sponsors/Logo_Rario_1.png",
-    alt: "Rario",
+    imageUrl: "/sponsors/devfolio.png",
+    alt: "Devfolio",
+    link: "https://devfolio.co",
+  },
+  {
+    imageUrl: "/sponsors/polygon.png",
+    alt: "Polygon",
+    link: "https://polygon.technology",
   },
 ];
 
-/*
-
 const partners = [
+  // {
+  //   imageUrl: "/sponsors/balsamiq.png",
+  //   alt: "Balsamiq",
+  // },
+  // {
+  //   imageUrl: "/sponsors/rosenfield.png",
+  //   alt: "Rosenfeld",
+  // },
   {
-    imageUrl: "/sponsors/balsamiq.png",
-    alt: "Balsamiq",
+    imageUrl: "/sponsors/solana.png",
+    alt: "Solana",
+    link: "https://solana.com",
   },
   {
-    imageUrl: "/sponsors/rosenfield.png",
-    alt: "Rosenfeld",
+    imageUrl: "/sponsors/filecoin.png",
+    alt: "Filecoin",
+    link: "https://filecoin.io",
   },
   {
     imageUrl: "/sponsors/replit.png",
     alt: "Repl.it",
+    link: "https://replit.com",
   },
-  {
-    imageUrl: "/sponsors/wolfram.png",
-    alt: "Wolfram",
-  },
-  {
-    imageUrl: "/sponsors/aops.png",
-    alt: "AOPS",
-  },
-  {
-    imageUrl: "/sponsors/taskade.png",
-    alt: "Taskade",
-  },
-  {
-    imageUrl: "/sponsors/interview-cake.svg",
-    alt: "Interview Cake",
-  },
-  {
-    imageUrl: "/sponsors/streamyard.png",
-    alt: "StreamYard",
-  },
+  // {
+  //   imageUrl: "/sponsors/wolfram.png",
+  //   alt: "Wolfram",
+  // },
+  // {
+  //   imageUrl: "/sponsors/aops.png",
+  //   alt: "AOPS",
+  // },
+  // {
+  //   imageUrl: "/sponsors/taskade.png",
+  //   alt: "Taskade",
+  // },
+  // {
+  //   imageUrl: "/sponsors/interview-cake.svg",
+  //   alt: "Interview Cake",
+  // },
+  // {
+  //   imageUrl: "/sponsors/streamyard.png",
+  //   alt: "StreamYard",
+  // },
   {
     imageUrl: "/sponsors/ndss.png",
     alt: "New Delhi Space Society",
+    link: "https://newdelhi.nss.org",
   },
 ];
-*/
+
+function SponsorImage({ imageUrl, alt }) {
+  return (
+    <img src={imageUrl} alt={alt} className="h-[70px] w-auto object-contain" />
+  );
+}
 
 export default function Sponsors() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      <h2 className="text-4xl font-bold text-accent-dark">Our Sponsors</h2>
-      <div className="flex flex-wrap items-center justify-center mt-5">
-        {sponsors.map(({ imageUrl, alt }, i) => (
-          <img
-            src={imageUrl}
-            alt={alt}
-            key={i}
-            className="h-[100px] w-auto mx-10 mb-5"
-          />
-        ))}
+      {/* <h2 className="text-4xl font-bold text-accent-dark">Our Sponsors</h2> */}
+      <div className="flex flex-wrap items-center justify-center mt-5 gap-4">
+        {sponsors.map(({ imageUrl, alt, link }, i) =>
+          link ? (
+            <a href={link} target="_blank" rel="noreferrer">
+              <SponsorImage imageUrl={imageUrl} alt={alt} />
+            </a>
+          ) : (
+            <SponsorImage imageUrl={imageUrl} alt={alt} />
+          )
+        )}
       </div>
-      <h2 className="text-xl font-semibold text-accent-dark mt-10">
-        Sponsors for IT@CE Fest 2022
-      </h2>
+      <h2 className="text-4xl font-bold text-accent-dark mt-4">Partners</h2>
+      <div className="flex flex-wrap items-center justify-center mt-5 gap-4">
+        {partners.map(({ imageUrl, alt, link }, i) =>
+          link ? (
+            <a href={link} target="_blank" rel="noreferrer">
+              <SponsorImage imageUrl={imageUrl} alt={alt} />
+            </a>
+          ) : (
+            <SponsorImage imageUrl={imageUrl} alt={alt} />
+          )
+        )}
+      </div>
     </div>
   );
 }
