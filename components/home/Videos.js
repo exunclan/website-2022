536f4b0e2fe738270492c76efefd35380f2d849a
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2022-11-10 17:38:03 +0530

    chore: add key prop to child elements

--- components/home/Videos.js
+++ components/home/Videos.js
@@ -5,25 +5,33 @@ export default function Videos({ aboutRef }) {
   return (
     <>
       <div className="w-full" ref={aboutRef}>
-        {videoData.map((element) => {
+        {videoData.map((element, index) => {
           return (
             <>
-              <h2 className="text-4xl mt-24 text-primary text-left font-bold">
+              <h2
+                key={index}
+                className="text-4xl mt-24 text-primary text-left font-bold"
+              >
                 Exun {element.year} Videos
               </h2>
-              <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2">
-                {element.data.map((item) => {
+              <div
+                key={index}
+                className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2"
+              >
+                {element.data.map((item, i) => {
                   return (
                     <>
-                      <div className="flex flex-col p-6 mx-2 sm:mx-12">
+                      <div key={i} className="flex flex-col p-6 mx-2 sm:mx-12">
                         <img
                           className="object-cover h-52 lg:h-64 rounded-t"
                           src={`https://img.youtube.com/vi/${
                             item.link.split("v=")[1]
                           }/hqdefault.jpg`}
                           alt="Video Image"
+                          key={i}
                         />
                         <a
+                          key={i}
                           href={item.link}
                           className="bg-blue-500 inline-block text-white font-semibold rounded-b cursor-pointer py-4 text-center"
                         >
