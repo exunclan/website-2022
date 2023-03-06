3f422267826433eeb25180259b151c9a9a0610c2
Kavin Valli <mail@kavin.me>
2023-03-06 22:09:43 +0530

    chore!: use nextjs image in projects
    BREAKING CHANGE: update to nextjs 13

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,9 +1,24 @@
+import Image from "next/image";
+
 export default function ProjectCard({ title, link, bg }) {
   return (
-    <div style={{backgroundImage: `url(${bg})`}} className="bg-cover bg-center w-90 h-56 rounded-md flex items-center justify-center">
-      <a className="rounded-md w-full h-full flex items-center justify-center transition ease-in-out duration-500 opacity-0 hover:bg-black/60 hover:opacity-100" href={link} target="_blank" rel="noopener noreferrer">
+    // <div style={{backgroundImage: `url(${bg})`}} className="bg-cover bg-center w-90 h-56 rounded-md flex items-center justify-center">
+    <div className="w-90 h-56 rounded-md flex items-center justify-center relative">
+      <Image
+        src={bg}
+        alt={`${title}'s image`}
+        fill
+        className="top-0 left-0 object-cover overflow-hidden"
+        sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw,33vw"
+      />
+      <a
+        className="rounded-md w-full h-full flex items-center justify-center transition ease-in-out duration-500 opacity-0 hover:bg-black/60 hover:opacity-100 z-10"
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <h4 className="text-[#F4F8FF] text-xl font-semibold">{title}</h4>
       </a>
     </div>
-  )
+  );
 }
--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Head from "next/head";
 import HeadingWithIllustration from "../components/HeadingWithIllustration";
 import projects from "../data/projects.json";
@@ -11,7 +11,7 @@ export default function Projects() {
     let arr = showMore;
     arr[index] = !arr[index];
     setShowMore([...arr]);
-  }
+  };
   return (
     <>
       <Head>
@@ -23,62 +23,70 @@ export default function Projects() {
           description="A collection of a few of the most innovative projects made by Exun's elite team of Members in disciplines ranging from 3D Design to Competitive Programming"
         />
         {projects.map((project, index) => (
-          <>
-           <HeadingWithIllustration key={project.type} title={project.type} />
-           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
-            {project.content.slice(0, showMore[index] ? project.content.length : 3).map(({title, link}) => (
-              <ProjectCard key={title} title={title} link={link} bg={`/projects/${title.replace(/ /g, '%20')}.png`}/>
-            ))}
-           </div>   
-           {project.content.length > 3 ? (
-            <a
-              className="text-left w-full flex justify-end mt-6 text-accent cursor-pointer"
-              target="_blank"
-              rel="noreferrer"
-              onClick={() => toggleButton(index)}
-            >            
-            {showMore[index] ? (
-              <>
-              <span>View Less</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </>
-            ) : (
-              <>
-                <span>View More</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 ml-1"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          <section key={index}>
+            <HeadingWithIllustration key={project.type} title={project.type} />
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+              {project.content
+                .slice(0, showMore[index] ? project.content.length : 3)
+                .map(({ title, link }) => (
+                  <ProjectCard
+                    key={title}
+                    title={title}
+                    link={link}
+                    bg={`/projects/${title.replace(/ /g, "%20")}.png`}
                   />
-                </svg>
-              </>
+                ))}
+            </div>
+            {project.content.length > 3 ? (
+              <a
+                className="text-left w-full flex justify-end mt-6 text-accent cursor-pointer"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => toggleButton(index)}
+              >
+                {showMore[index] ? (
+                  <>
+                    <span>View Less</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </>
+                ) : (
+                  <>
+                    <span>View More</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6 ml-1"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </>
+                )}
+              </a>
+            ) : (
+              <></>
             )}
-            </a>
-           ) : (<></>)}                   
-          </>
-        ))
-        }
+          </section>
+        ))}
       </div>
     </>
   );
