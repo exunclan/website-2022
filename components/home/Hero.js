5a63cc84b638d7e68b60e233ff063036e08ad3bf
ShoneUnity <e10789akansh@dpsrkp.net>
2023-11-14 21:12:19 +0530

    Adding a test file to new_branch

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -27,7 +27,7 @@ export default function Hero({ scrollToAboutRef }) {
       </div>
       <Image
         className="z-[1]"
-        src="/hero.png"
+        src="/illus.PNG"
         width={800}
         height={500}
         alt="Hero Image"
