b71bb754f689e37b0d5c5bbfe8e76f530a09c319
Atharv varshney <Atharvvarshney07@gmail.com>
2024-10-19 00:42:26 +0530

    Updating Hero section for Exun 2024

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -2,34 +2,33 @@ import Image from "next/image";
 
 export default function Hero({ scrollToAboutRef }) {
   return (
-    <div className="w-full flex flex-col-reverse md:flex-row justify-between items-center py-20 px-2">
+    <div className="w-full flex flex-col md:flex-row justify-between items-center gap-20 py-10 px-2">
       <div className="flex flex-col items-center md:items-start">
-        <h1 className="text-5xl md:text-6xl mt-6 md:mt-0 text-accent font-semibold leading-tight md:leading-tight text-center md:text-left">
-          Celebrating
-          <br /> 31 years of
-          <br /> <span className="text-primary">Exun</span>
+        <h1 className="text-5xl md:text-7xl mt-6 md:mt-0 text-accent font-semibold leading-tight md:leading-tight text-center md:text-left">
+          Celebrating 32 years
+          <br /> of <span className="text-primary">Exun</span>
         </h1>
-        <div className="flex items-center gap-3 mt-5">
-          {/*
-          <button className="button" onClick={scrollToAboutRef}>
+        <div className="flex items-center gap-3 mt-10 md:mt-20">
+          
+          {/* <button className="button" onClick={scrollToAboutRef}>
             Read More
-          </button>
-          */}
+          </button> */}
+         
           <a
             href="https://reg.exunclan.com"
-            className="button"
+            className="button text-lg font-medium"
             target="_blank"
             rel="noreferrer"
           >
-            Register for Exun 2023
+            Register for Exun 2024
           </a>
         </div>
       </div>
       <Image
         className="z-[1]"
-        src="/illus.PNG"
-        width={800}
-        height={500}
+        src="/illus2024.png"
+        width={350}
+        height={620}
         alt="Hero Image"
       />
     </div>
--- components/home/Sponsors.js
+++ components/home/Sponsors.js
@@ -2,74 +2,14 @@ import { useState, useEffect } from "react";
 
 const sponsors = [
     {
-        imageUrl: "/sponsors/khulke.png",
-        alt: "Khulke",
-        link: "https://www.khulke.com/",
+        imageUrl: "/sponsors/athena.png",
+        alt: "Athena Education",
+        link: "https://www.athenaeducation.co.in/",
         height: "200px",
-    },
-    {
-        imageUrl: "/sponsors/Safexpress_Logo.png",
-        alt: "Safexpress",
-        link: "https://www.safexpress.com/",
-        height: "200px",
-    },
-    {
-        imageUrl: "/sponsors/plaksha.png",
-        alt: "Plaksha",
-        link: "https://plaksha.edu.in/",
-        height: "130px",
     },
 ];
 
-const partners = [
-    {
-        imageUrl: "/sponsors/rosenfield.png",
-        alt: "Rosenfeld",
-        link: "https://rosenfeldmedia.com/",
-        height: "90px",
-    },
-    {
-        imageUrl: "/sponsors/echo3d.png",
-        alt: "Echo 3D",
-        link: "https://echo3d.co",
-    },
-    {
-        imageUrl: "/sponsors/interview-cake.png",
-        alt: "Interview Cake",
-        link: "https://www.interviewcake.com/",
-        height: "70px",
-    },
-    {
-        imageUrl: "/sponsors/xyz.png",
-        alt: ".xyz Domains",
-        link: "https://gen.xyz",
-        height: "70px",
-    },
-    {
-        imageUrl: "/sponsors/cubelelo.png",
-        alt: "Cubelelo",
-        link: "https://www.cubelelo.com/",
-        height: "160px",
-    },
-    {
-        imageUrl: "/sponsors/wolfram.png",
-        alt: "Wolfram Language",
-        link: "https://www.wolframalpha.com/",
-        height: "90px",
-    },
-    {
-        imageUrl: "/sponsors/playstation.png",
-        alt: "PlayStation",
-        link: "https://www.playstation.com/",
-        height: "70px",
-    },
-    {
-        imageUrl: "/sponsors/aops.png",
-        alt: "AOPS",
-        link: "https://artofproblemsolving.com/",
-        height: "100px",
-    },
-];
+const partners = [];
 
 function SponsorImage({ key, imageUrl, alt, img_height }) {
     return (
@@ -115,21 +55,27 @@ export default function Sponsors() {
                 ))}
             </div>
 
-            <h1 className="text-3xl md:text-6xl font-bold text-accent-dark my-8 mt-[130px]">
-                Community Partners
-            </h1>
-            <div className="flex flex-wrap items-center justify-center mt-5 gap-8">
-                {partners.map(({ imageUrl, alt, link, height }, i) => (
-                    <a key={i} href={link} target="_blank" rel="noreferrer">
-                        <SponsorImage
-                            key={i}
-                            imageUrl={imageUrl}
-                            alt={alt}
-                            img_height={height ? height : ImgHeight}
-                        />
-                    </a>
-                ))}
-            </div>
+            {
+                partners && partners.length
+                    ? <>
+                        <h1 className="text-3xl md:text-6xl font-bold text-accent-dark my-8 mt-[130px]">
+                            Community Partners
+                        </h1>
+                        <div className="flex flex-wrap items-center justify-center mt-5 gap-8">
+                            {partners.map(({ imageUrl, alt, link, height }, i) => (
+                                <a key={i} href={link} target="_blank" rel="noreferrer">
+                                    <SponsorImage
+                                        key={i}
+                                        imageUrl={imageUrl}
+                                        alt={alt}
+                                        img_height={height ? height : ImgHeight}
+                                    />
+                                </a>
+                            ))}
+                        </div>
+                    </>
+                    : null
+            }
         </div>
     );
 }
